1e48a478705a9c7f5ac14dff32022ec670c0e8a2
rohit-541 <rohitkumar240iit@gmail.com>
2025-01-14 09:02:40 +0530

    Minor changes

--- Public/index.js
+++ Public/index.js
@@ -2,7 +2,7 @@
 //connect to chat server
 let socket;
 try {
-    socket = io.connect('http://localhost:3000/');
+    socket = io.connect('https://chatterup-o8xb.onrender.com/');
 } catch (error) {
     alert('Please try later');
 }
