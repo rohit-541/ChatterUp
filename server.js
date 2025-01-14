6b26fa12c76dd09217779a874c2686ba8fbcd510
rohit-541 <rohitkumar240iit@gmail.com>
2025-01-14 09:30:42 +0530

    Start Command added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import  dotenv from 'dotenv'
 dotenv.config();
 import {server} from './index.js'
-import { connectToDataBase } from './Configuration/Database/Connection.js';
+// import { connectToDataBase } from './Configuration/Database/Connection.js';
 
 server.listen(process.env.PORT||3000,async ()=>{
     console.log("Server is listening");
     //connect to database
-    await connectToDataBase();
+    // await connectToDataBase();
 });
